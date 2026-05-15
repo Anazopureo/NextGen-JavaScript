70a9a3999cf702eda994736f4ab617e83e378fa1
IyanuOluwa <203460167+Tobi-Monarch1405@users.noreply.github.com>
2026-05-15 15:20:38 +0100

    prompt for collecting data from users and comparing with constant provided and gives a "correct" or "wrong " as response using alert

--- Nigerian Quiz.js
+++ Nigerian Quiz.js
@@ -1,11 +1,55 @@
+// Run in a browser
 const answers = {
     ques1 : 1960,
-    ques2 : "River Niger",
+    ques2 : "RIVER NIGER",
     ques3 : "NECOM HOUSE",
-    ques4 : "First Storey Building",
+    ques4 : "FIRST STOREY BUILDING",
     ques5 : 65,
 
 };
 
+// Input and display name
+var name= prompt("What is your Name");
+alert("Hello " + name + ", Lets begin ")
 
-prompt("What s your Name");
+    var Question1 = prompt("What year did Nigeria gain her independence?"); //request data
+
+if (Question1 == answers.ques1){   //checks data with given constant
+    alert("correct")
+}else{
+    alert("wrong")
+};
+
+var Question2 = prompt("what is the Longest River in Nigeria?").toUpperCase();
+// let Question2 = Question2;
+if (Question2 == answers.ques2){
+    alert("correct")
+}else{
+    alert("wrong")
+};
+
+var Question3 = prompt("What is the tallest building in Nigeria?").toUpperCase();
+if (Question2 == answers.ques2){
+    alert("correct")
+}else{
+    alert("wrong")
+};
+
+
+var Question4 = prompt("What is the Oldest builing in Nigeria?").toUpperCase();
+if (Question4 == answers.ques4){
+    alert("correct")
+}else{
+    alert("wrong")
+};
+
+var Question5 = prompt("What year did Nigeria gain her independence?").toUpperCase();
+if (Question5 == answers.ques5){
+    alert("correct")
+}else{
+    alert("wrong")
+};
+
+
+
+
