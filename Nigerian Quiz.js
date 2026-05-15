febc16f982b3346ae8ac7243b67920a7868c1297
IyanuOluwa <203460167+Tobi-Monarch1405@users.noreply.github.com>
2026-05-15 11:40:44 +0100

    declare constants

---
+++ Nigerian Quiz.js
@@ -0,0 +1,11 @@
+const answers = {
+    ques1 : 1960,
+    ques2 : "River Niger",
+    ques3 : "NECOM HOUSE",
+    ques4 : "First Storey Building",
+    ques5 : 65,
+
+};
+
+
+prompt("What s your Name");
